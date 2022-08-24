c08ecb223581868770f97232c7f9fdea46c157fa
m4nzm333 <irman.mashuri@gmail.com>
2022-08-24 09:16:50 +0800

    Update environtment

--- services/asap/app.py
+++ services/asap/app.py
@@ -4,14 +4,21 @@ import json
 from bson.json_util import dumps, loads
 from bson import ObjectId
 from datetime import timezone, datetime, timedelta
+import os
 
 app = Flask(__name__)
 
-host = '127.0.0.1:27017'
+# Get environment variables
+DB_HOSTNAME = os.getenv('DB_HOSTNAME', default='127.0.0.1')
+DB_PORT = os.getenv('DB_PORT', default=27017)
+DB_USERNAME = os.getenv('DB_USERNAME', default='root')
+DB_PASSWORD = os.getenv('DB_PASSWORD', default='123456')
+DB_NAME = os.getenv('DB_NAME', default='myiot')
 
-myclient = pymongo.MongoClient(
-    "mongodb://root:123456@{host}/".format(host=host))
-mydb = myclient["myiot"]
+
+myclient = pymongo.MongoClient("mongodb://{username}:{password}@{hostname}:{port}/".format(
+    hostname=DB_HOSTNAME, port=DB_PORT, username=DB_USERNAME, password=DB_PASSWORD))
+mydb = myclient[DB_NAME]
 
 
 @app.route('/')
