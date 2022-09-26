3b5838421a33e14ea855666a42dccb9ce59a86af
m4nzm333 <irman.mashuri@gmail.com>
2022-09-26 10:35:26 +0800

    Update scheduled

--- services/scheduler/app.py
+++ services/scheduler/app.py
@@ -1,11 +1,11 @@
+import requests
+import json
+from datetime import datetime
+from time import sleep
 print("===============")
 print("Trigger Service")
 print("===============\n")
 
-from time import sleep
-from datetime import datetime
-import json
-import requests
 
 while True:
     print('Ini adalah scheduler')
@@ -20,25 +20,27 @@ while True:
     for schedule in allScheduler:
 
         if schedule['jenis']['tipe'] == 'harian':
-            print(schedule)
 
             timeTrigger = schedule['jenis']['jam']
             idDevice = schedule['control']['id']
             value = schedule['value']
 
             now = datetime.now()
-            print(now)
             trigger = datetime.strptime('{} {}'.format(
                 now.strftime("%Y-%m-%d"), timeTrigger), "%Y-%m-%d %H:%M")
 
-            # TODO: Cek history
+            control = requests.get(
+                'http://api-control:8080/getById', params={"id": idDevice})
+            control = control.json()
 
-            # Toleransi schedule di bawah 5 menit
-            if now > trigger and ((now - trigger).total_seconds() / 60.0 <= 5):
-                print('set new status {control} to = {value}'.format(
-                    control=schedule['control']['nama'], value=schedule['value']))
-                print(idDevice)
+            if control:
+                if control['value'] != schedule['value']:
+                    # Toleransi schedule di bawah 5 menit
+                    if now > trigger and ((now - trigger).total_seconds() / 60.0 <= 5):
+                        print('set new status {control} to = {value}'.format(
+                            control=schedule['control']['nama'], value=schedule['value']))
+                        print(idDevice)
 
-                requests.get('http://api-control:8080/setValue',
-                             params={'id': idDevice, 'trigger': 'scheduled', 'value': schedule['value']}, verify=False)
+                        requests.get('http://api-control:8080/setValue',
+                                     params={'id': idDevice, 'trigger': 'scheduled', 'value': schedule['value']}, verify=False)
     sleep(5)
