12a5b665b10335feb73681493ea3e1210b4ad0a6
m4nzm333 <irman.mashuri@gmail.com>
2022-09-26 10:25:24 +0800

    Fix trigger ndk jalan terus

--- services/trigger/app.py
+++ services/trigger/app.py
@@ -23,32 +23,39 @@ while True:
         lastData = requests.get(
             'http://api-sensor:8080/getLastDataById', params={"id": idSensor})
         lastData = lastData.json()
-        # If Last data Exist
-        if lastData:
-            # Toleransi last data sebesar 5 menit sebelumnya
-            lastDataTime = datetime.strptime(
-                lastData['ts'], '%Y-%m-%d %H:%M:%S')
-            now = datetime.now()
 
-            if now > lastDataTime and ((now - lastDataTime).total_seconds() / 60.0 <= 5):
-                # Jika Jenis Lebih Dari
-                if jenis == 'more than' and lastData['value'] >= sensorValue:
-                    print("Trigger {sensor} {currentValue} {jenis} {sensorData} set {control} to {controlValue}".format(
-                        sensor=idSensor, currentValue=lastData['value'], jenis=jenis, sensorData=sensorValue, control=idControl, controlValue=controlValue))
-                    requests.get('http://api-control:8080/setValue',
-                                params={'id': idControl, 'trigger': 'triggered', 'value': controlValue}, verify=False)
-                # Jika Jenis Sama Dengan
-                if jenis == 'equal' and lastData['value'] == sensorValue:
-                    requests.get('http://api-control:8080/setValue',
-                                params={'id': idControl, 'trigger': 'triggered', 'value': controlValue}, verify=False)
-                    print("Trigger {sensor} {currentValue} {jenis} {sensorData} set {control} to {controlValue}".format(
-                        sensor=idSensor, currentValue=lastData['value'], jenis=jenis, sensorData=sensorValue, control=idControl, controlValue=controlValue))
-                # Jika Jenis Kurang Dari
-                if jenis == 'less than' and lastData['value'] <= sensorValue:
-                    requests.get('http://api-control:8080/setValue',
-                                params={'id': idControl, 'trigger': 'triggered', 'value': controlValue}, verify=False)
-                    print("Trigger {sensor} {currentValue} {jenis} {sensorData} set {control} to {controlValue}".format(
-                        sensor=idSensor, currentValue=lastData['value'], jenis=jenis, sensorData=sensorValue, control=idControl, controlValue=controlValue))
-    
+        control = requests.get(
+            'http://api-control:8080/getById', params={"id": idControl})
+        control = control.json()
+
+        # If Last data Exist
+        if lastData and control:
+
+            if control['value'] != trigger['value']:
+                # Toleransi last data sebesar 5 menit sebelumnya
+                lastDataTime = datetime.strptime(
+                    lastData['ts'], '%Y-%m-%d %H:%M:%S')
+                now = datetime.now()
+
+                if now > lastDataTime and ((now - lastDataTime).total_seconds() / 60.0 <= 5):
+                    # Jika Jenis Lebih Dari
+                    if jenis == 'more than' and lastData['value'] >= sensorValue:
+                        print("Trigger {sensor} {currentValue} {jenis} {sensorData} set {control} to {controlValue}".format(
+                            sensor=idSensor, currentValue=lastData['value'], jenis=jenis, sensorData=sensorValue, control=idControl, controlValue=controlValue))
+                        requests.get('http://api-control:8080/setValue',
+                                     params={'id': idControl, 'trigger': 'triggered', 'value': controlValue}, verify=False)
+                    # Jika Jenis Sama Dengan
+                    if jenis == 'equal' and lastData['value'] == sensorValue:
+                        requests.get('http://api-control:8080/setValue',
+                                     params={'id': idControl, 'trigger': 'triggered', 'value': controlValue}, verify=False)
+                        print("Trigger {sensor} {currentValue} {jenis} {sensorData} set {control} to {controlValue}".format(
+                            sensor=idSensor, currentValue=lastData['value'], jenis=jenis, sensorData=sensorValue, control=idControl, controlValue=controlValue))
+                    # Jika Jenis Kurang Dari
+                    if jenis == 'less than' and lastData['value'] <= sensorValue:
+                        requests.get('http://api-control:8080/setValue',
+                                     params={'id': idControl, 'trigger': 'triggered', 'value': controlValue}, verify=False)
+                        print("Trigger {sensor} {currentValue} {jenis} {sensorData} set {control} to {controlValue}".format(
+                            sensor=idSensor, currentValue=lastData['value'], jenis=jenis, sensorData=sensorValue, control=idControl, controlValue=controlValue))
+
     # Delay selama 5 detik
     sleep(5)
